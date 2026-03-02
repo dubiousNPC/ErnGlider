25c3df5be4db7b8e546bc96defdbcde07e7a6de3
Erin Pentecost <erin@pentecost.email>
2026-03-01 16:19:11 -0800

    new animations

---
+++ 01 RacerGlider/scripts/ErnGlider/glideranim.lua
@@ -0,0 +1,23 @@
+return {
+    basic = {
+        forward = "GliderIdle",
+        right = "GliderIdle",
+        left = "GliderIdle",
+        bone = "Neck",
+        model = nil
+    },
+    advanced = {
+        forward = "GliderIdle",
+        right = "GliderIdle",
+        left = "GliderIdle",
+        bone = "Neck",
+        model = nil
+    },
+    masterwork = {
+        forward = "GliderIdle",
+        right = "GliderIdle",
+        left = "GliderIdle",
+        bone = "Neck",
+        model = nil
+    },
+}
