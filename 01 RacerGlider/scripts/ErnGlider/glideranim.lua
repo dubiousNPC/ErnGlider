6a88c4ad2ef4c4aaccbdffe051d7e4834d6509c1
Erin Pentecost <erin@pentecost.email>
2026-03-06 18:46:31 -0800

    add new glider

--- 01 RacerGlider/scripts/ErnGlider/glideranim.lua
+++ 01 RacerGlider/scripts/ErnGlider/glideranim.lua
@@ -4,20 +4,20 @@ return {
         right = "GliderIdle",
         left = "GliderIdle",
         bone = "Neck",
-        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
+        model = "Meshes\\ErnGlider\\RacerGlider204.nif"
     },
     advanced = {
         forward = "GliderIdle",
         right = "GliderIdle",
         left = "GliderIdle",
         bone = "Neck",
-        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
+        model = "Meshes\\ErnGlider\\RacerGlider204.nif"
     },
     masterwork = {
         forward = "GliderIdle",
         right = "GliderIdle",
         left = "GliderIdle",
         bone = "Neck",
-        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
+        model = "Meshes\\ErnGlider\\RacerGlider204.nif"
     },
 }
