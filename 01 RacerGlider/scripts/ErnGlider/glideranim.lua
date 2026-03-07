dab3dda5b8fa7a345372244efa3210de13ec9ace
Erin Pentecost <erin@pentecost.email>
2026-03-06 18:19:44 -0800

    racer glider

--- 01 RacerGlider/scripts/ErnGlider/glideranim.lua
+++ 01 RacerGlider/scripts/ErnGlider/glideranim.lua
@@ -3,21 +3,21 @@ return {
         forward = "GliderIdle",
         right = "GliderIdle",
         left = "GliderIdle",
-        bone = "Groin",
-        model = "meshes/r/netch_betty.nif"
+        bone = "Neck",
+        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
     },
     advanced = {
         forward = "GliderIdle",
         right = "GliderIdle",
         left = "GliderIdle",
-        bone = "Groin",
-        model = "meshes/r/netch_betty.nif"
+        bone = "Neck",
+        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
     },
     masterwork = {
         forward = "GliderIdle",
         right = "GliderIdle",
         left = "GliderIdle",
-        bone = "Groin",
-        model = "meshes/r/netch_betty.nif"
+        bone = "Neck",
+        model = "Meshes\\ErnGlider\\dbsRacerGlider1.nif"
     },
 }
